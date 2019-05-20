d3666e30e6ab9a3bbded8de47f227b2580914bae
Luke Park <twodude@naver.com>
2019-05-20 11:30:12 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,31 +1,19 @@
-// Copyright 2017 https://tokenmarket.net - MIT licensed
-//
-// Run with Node 7.x as:
-//
-// node --harmony-async-await  deploy.js
-//
-
 let fs = require("fs");
-let Web3 = require('web3'); // https://www.npmjs.com/package/web3
+let Web3 = require('web3'); // npm install web3@0.19
 
 // Create a web3 connection to a running geth node over JSON-RPC running at
 // http://localhost:8545
-// For geth VPS server + SSH tunneling see
-// https://gist.github.com/miohtama/ce612b35415e74268ff243af645048f4
 let web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 // Read the compiled contract code
 // Compile with
-// solc SampleContract.sol --combined-json abi,asm,ast,bin,bin-runtime,clone-bin,devdoc,interface,opcodes,srcmap,srcmap-runtime,userdoc > contracts.json
+// build/solc/solc realEthash.sol --combined-json abi,asm,ast,bin,bin-runtime,devdoc,interface,opcodes,srcmap,srcmap-runtime,userdoc > contracts.json
 let source = fs.readFileSync("./solexam/contracts.json");
 let contracts = JSON.parse(source)["contracts"];
 
-// ABI description as JSON structure
-let abi = JSON.parse(contracts["realEthash.sol:realEthash"].abi);
-
-// Smart contract EVM bytecode as hex
-let code = '0x' + contracts["realEthash.sol:realEthash"].bin;
+let abi = JSON.parse(contracts["realEthash.sol:realEthash"].abi); // ABI description as JSON structure
+let code = '0x' + contracts["realEthash.sol:realEthash"].bin; // Smart contract EVM bytecode as hex
 
 // Create Contract proxy class
 let SampleContract = web3.eth.contract(abi);
@@ -34,7 +22,6 @@ let SampleContract = web3.eth.contract(abi);
 const ADDRESS = "0x6282ad5f86c03726722ec397844d2f87ced3af89";
 // const PRIVATE_KEY = "75F8343E57BD410673A4A770C25EA7651CB8CD6BC3068DDF915945BDA320A134";
 // let privateKey = new Buffer(PRIVATE_KEY, 'hex')
-
 console.log("Unlocking coinbase account");
 var password = "12341234";
 try {
@@ -45,15 +32,14 @@ try {
 }
 
 console.log("Deploying the contract");
-let contract = SampleContract.new({ from: web3.eth.coinbase, gas: 1000000, data: code });
+let contract = SampleContract.new({
+  from: web3.eth.coinbase,
+  gas: 1000000,
+  data: code
+});
 
 // Transaction has entered to geth memory pool
 console.log("Transaction Hash: \"" + contract.transactionHash + "\"");
-
-// http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 // We need to wait until any miner has included the transaction
 // in a block to get the address of the contract
@@ -64,9 +50,14 @@ async function waitBlock() {
       console.log("Contract Address: \"" + receipt.contractAddress + "\"");
       break;
     }
-    console.log("Waiting a mined block to include your contract... currently in block " + web3.eth.blockNumber);
-    await sleep(4000);
+    console.log("Waiting a mined block to include your contract... latest block is " + web3.eth.blockNumber);
+    await sleep(5000);
   }
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// main
 waitBlock();
